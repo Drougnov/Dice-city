ee41464d29939817aa05710c3c442851464b5cc3
RockyBaura <251drougnov@gmail.com>
2022-10-02 10:00:42 +0600

    Markup added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 export default function App(){
   return(
-    <div className='container'>
-      <p>Working fine</p>
+    <div className='board'>
+      <div className="counter">
+        <div className="counter__time">
+          <i class="fa-solid fa-stopwatch"></i>
+          <span>01:00:00</span>
+        </div>
+        <div className="counter__roll">
+          <i class="fa-solid fa-dice"></i>
+          <span>15</span>
+        </div>
+      </div>
+      <div className="die-container">
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+        <i class="fa-solid fa-dice-one"></i>
+      </div>
+      <div className="btns">
+        <button><i class="fa-solid fa-volume-xmark"></i><i class="fa-solid fa-volume-high"></i></button>
+        <button><i class="fa-solid fa-dice"></i></button>
+        <button><i class="fa-solid fa-moon"></i><i class="fa-solid fa-sun"></i></button>
+      </div>
     </div>
   )
 }
